51bc3224043375702efbf46deacf9316236d8166
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2023-09-10 15:26:38 -0500

    review what you know d1js
    day 1 js review exercises

---
+++ js-ex-review/maind1rev.js
@@ -0,0 +1,42 @@
+//alert('hello potential clients');
+
+// let urName='Garrett';
+// console.log(urName);
+
+// function calculateArea(l, w){
+// return console.log(l*w);}
+// calculateArea(2,4); // 8
+
+
+// function checkOddOrEven(n) {
+//   if (n & 1 === 1) {
+//       return "Number is odd";
+//   }
+//   return "Number is even";
+// }
+
+// console.log(checkOddOrEven(5));
+// console.log(checkOddOrEven(6));
+// console.log(checkOddOrEven(12));
+
+// for (let i=1;i <= 10;i++){
+//   console.log(i);
+// }
+
+// const name = prompt('enter your name: ' );
+// if (nameIs = name);
+// console.log('hello'+ ' ' + nameIs);
+
+// function sumOfAll(){
+//  const a = 100;
+// if i=0; i<a; i++)
+// console.log(i);}
+
+// let a = prompt('num1');
+// let b = prompt('num2');
+// function arithmatic(a, b){
+// return console.log((2+4), (2-4), (2*4), (2/4));
+// }
+// arithmatic();
+// console.log 
+
