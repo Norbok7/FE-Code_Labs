ed83270659d7ea8dfc6b8f64e4434f06ae5a20fb
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2023-09-12 14:14:38 -0500

    Delete else-if-practice directory

--- else-if-practice/node.js
+++
@@ -1,8 +0,0 @@
-// say gm 6-12 say ge 12-18 else say gn
-let hour = 19;
-if (hour >= 6 && hour <12)
-console.log('gm');
-else if ( hour >=12 && hour <18)
-console.log('ga');
-else 
-console.log('gn');
