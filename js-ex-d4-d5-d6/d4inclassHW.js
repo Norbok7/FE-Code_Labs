0a1e1c88d3fc76805335e8114081230fdacd1464
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2023-09-15 13:44:22 -0500

    d4 of js HW
    working to finish assignement did ex. 2,3,4

--- js-ex-d4-d5-d6/d4inclassHW.js
+++ js-ex-d4-d5-d6/d4inclassHW.js
@@ -23,46 +23,44 @@
 // const user = new User();
 // console.log(user);
 /////////////////EX. 2//////////////////////
+
+
+/// step 1.Define a Person class with properties name and age.
 class Person  {
   constructor(name, age) {
   this.name = name;
   this.age = age;
-  
-}
- 
+  }
+//step 2. Add a method greet() to this class which outputs a greeting.
 greet() {
   console.log(`Hello! my name is ${this.name} I am ${this.age}`)
   }
-  static info () {
-    return console.log('this is a Person class');
-  }
-  set ageNewOne(value){
-    return (age<=2)}
+  
+static info(){
+  return 'this is a person class'
+}
 
-    
-  get ageNew() {
-    return this.age}
-  }
+
+}
+
+//step 3. Instantiate two different Person objects and invoke their greet methods.
+//instantiate refers to using a class typically
+const bio = new Person('joe', 24);
+const bioTwo = new Person('sarah', 22); // person objects instatiated from a class
+bio.greet();
+bioTwo.greet();
+console.log(bio.info);
+console.log(bio);
+
     //come back to this problem
-    
-    
-  
-  
-  
+    ////////////ex. 3////////////////
+   // Modify your Person class. The constructor should now accept and 
+   //      initialize name and age properties.
 
-console.log(Person.info());
-const guy = new Person ('garrett', 24)
-guy.greet();
-
-//////////Ex. 3
-const girl = new Person ('brandi', 22)
-girl.greet();
-
-console.log(guy, girl);
+ 
 ////////////ex. 4////////////
 // static in class says SyntaxError: Unexpected strict mode reserved word
 // in outside of instance class it is fine
-//cant call me.info(); have to call Person.info
 ///////////ex.5//////////
 //come back to this one
 
